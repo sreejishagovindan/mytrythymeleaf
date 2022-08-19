e269a863093ede19444493af1ef00600a48cfb3b
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-18 20:12:21 -0500

    new project

---
+++ src/main/java/org/launchcode/mytrythymeleaf/MytrythymeleafApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.mytrythymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytrythymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytrythymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/mytrythymeleaf/MytrythymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.mytrythymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytrythymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

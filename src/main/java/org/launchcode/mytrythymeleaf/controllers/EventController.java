3e90487501756085e1790f02f21c64a3cb68e0f7
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-18 22:38:50 -0500

    done thymeleaf intro

---
+++ src/main/java/org/launchcode/mytrythymeleaf/controllers/EventController.java
@@ -0,0 +1,24 @@
+package org.launchcode.mytrythymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+    @GetMapping
+    public String displayAllEvents(Model model){
+        List<String> events =new ArrayList<>();
+        events.add("code with pride");
+        events.add("Strange Loop");
+        events.add("Apple WWDC");
+        events.add("Spring One platform");
+        model.addAttribute("events",events);
+        return "events/index";
+    }
+}

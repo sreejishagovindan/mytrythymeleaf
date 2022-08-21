5071d48c288e7ed5b9c0dd4107615a2917203dc7
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-20 19:52:09 -0500

    added model class

--- src/main/java/org/launchcode/mytrythymeleaf/controllers/EventController.java
+++ src/main/java/org/launchcode/mytrythymeleaf/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.mytrythymeleaf.controllers;
 
+import org.launchcode.mytrythymeleaf.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,7 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static List<String> events =new ArrayList<>();
+    private static List<Event> events =new ArrayList<>();
     @GetMapping
     public String displayAllEvents(Model model){
        // List<String> events =new ArrayList<>();
@@ -30,7 +31,7 @@ public class EventController {
     }
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+        events.add(new Event(eventName));
         return "redirect:"
                 ;
     }
---
+++ src/main/java/org/launchcode/mytrythymeleaf/models/Event.java
@@ -0,0 +1,21 @@
+package org.launchcode.mytrythymeleaf.models;
+
+public class Event {
+    private String name;
+    public Event(String name){
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}

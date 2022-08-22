ac5c240b1553d71907f27e71ed170fef73f39a24
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-21 20:26:40 -0500

    added validation annotations

--- src/main/java/org/launchcode/mytrythymeleaf/models/Event.java
+++ src/main/java/org/launchcode/mytrythymeleaf/models/Event.java
@@ -1,15 +1,27 @@
 package org.launchcode.mytrythymeleaf.models;
 
+import org.springframework.boot.convert.DataSizeUnit;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
+    @Size(min=3,max=50 ,message="name must be between 3 and 50 characters")
+    @NotBlank(message="name cannot be blank")
     private String name;
+    @Size(max=500,message="description too long")
+
     private String description;
     private int id;
     private static int nextId=1;
-    public Event(String name, String description){
+    @Email(message="invalid email!! try again")
+    private String contactEmail;
+    public Event(String name, String description, String contactEmail){
         this.name=name;
         this.description=description;
+        this.contactEmail=contactEmail;
         this.id=nextId;
         nextId++;
     }
@@ -32,6 +44,14 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     @Override

2605e20a7167fb806efb281763c864ec556b695c
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-26 23:22:48 -0500

    done one to many relationship

--- src/main/java/org/launchcode/mytrythymeleaf/controllers/EventController.java
+++ src/main/java/org/launchcode/mytrythymeleaf/controllers/EventController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("events")
@@ -22,13 +23,24 @@ public class EventController {
     //findAll,save,findById
     //private static List<Event> events =new ArrayList<>();
     @GetMapping
-    public String displayAllEvents(Model model){
-       // List<String> events =new ArrayList<>();
-        //events.add("code with pride");
-        //events.add("Strange Loop");
-        //events.add("Apple WWDC");
-        //events.add("Spring One platform");
+    public String displayAllEvents(@RequestParam(required=false) Integer categoryId, Model model ){
+    if(categoryId==null){
+
+        model.addAttribute("title","Create Event");
         model.addAttribute("events", eventRepository.findAll());
+
+    }
+    else{
+        Optional<EventCategory> result=eventCategoryRepository.findById(categoryId);
+        if(result.isEmpty()){
+            model.addAttribute("title","Invalid category ID"+categoryId);
+        }else{
+           EventCategory category=result.get();
+           model.addAttribute("title","Events in category" +category.getName());
+           model.addAttribute("events",category.getEvents());
+        }
+
+    }
         return "events/index";
     }
     @GetMapping("create")
@@ -36,7 +48,7 @@ public class EventController {
         model.addAttribute("title","Create Event");
        model.addAttribute("event",new Event());
        model.addAttribute("eventCategories",eventCategoryRepository.findAll());
-       System.out.println("Size of Cat = " + eventCategoryRepository.findAll());
+
         return "events/create";
     }
     @PostMapping("create")
--- src/main/java/org/launchcode/mytrythymeleaf/models/EventCategory.java
+++ src/main/java/org/launchcode/mytrythymeleaf/models/EventCategory.java
@@ -3,7 +3,10 @@ package org.launchcode.mytrythymeleaf.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -11,6 +14,8 @@ public class EventCategory extends AbstractEntity {
 
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+    @OneToMany(mappedBy="eventCategory")
+    private final List<Event> events=new ArrayList<>();
 
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long")String name){
         this.name=name;
@@ -22,6 +27,10 @@ public class EventCategory extends AbstractEntity {
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
